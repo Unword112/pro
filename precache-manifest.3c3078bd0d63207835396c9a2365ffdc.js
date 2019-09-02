a314ce45317d9a169c2609b13b58b2428bbcc2eb
unknown <pokzanaha@gmail.com>
2019-09-02 12:12:12 +0700

    Updates

---
+++ precache-manifest.3c3078bd0d63207835396c9a2365ffdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee68274e7332b985c5d45f6236592890",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2736ac62922ed968c11b",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d0473b86980d0b527f5f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2736ac62922ed968c11b",
+    "url": "/static/js/2.378744dd.chunk.js"
+  },
+  {
+    "revision": "d0473b86980d0b527f5f",
+    "url": "/static/js/main.d6f470ab.chunk.js"
+  },
+  {
+    "revision": "39c88b77b4861397dad6",
+    "url": "/static/js/runtime~main.994c2026.js"
+  }
+]);
